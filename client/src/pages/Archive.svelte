fd4081826d849baf2a344dfb5433be6436098977
August Eymann <augusteymann@Regans-MacBook-Pro.local>
2020-08-18 13:48:08 -0700

    update

--- client/src/pages/Archive.svelte
+++ client/src/pages/Archive.svelte
@@ -2,13 +2,15 @@
     import { navigate } from "svelte-routing";
     import { onMount } from "svelte";
     import NavBar from '../components/Navbar.svelte'
-    
+    import { tick } from 'svelte';
+
 	let files;
     let userData;
     let archiveData = [];
     let image = 'http://localhost:8080/api/v1/uploads/'
     let currentImage = ''
-
+    let searchParam = ''
+    let currentFilter = 'Date'
     function getArchive() {
         fetch('http://localhost:8080/api/v1/archive', {
             headers: {
@@ -58,7 +60,23 @@
     function changeImage(data) {
         currentImage = image + data
     }
- 
+
+    function setFilter(newFilter) {
+        console.log(newFilter)
+        currentFilter = newFilter;
+    }
+
+    function filterList() {
+        if (searchParam === '') {
+            getArchive();
+        } else {
+        archiveData = archiveData.filter(function (e) {
+            let info = e[currentFilter.toLowerCase()]
+            return info.includes(searchParam)
+        });
+        }   
+    }
+    
 
 
 </script>
@@ -75,29 +93,29 @@
 
         <h2>Photo Archive 📸</h2>
 
-        <button class="uk-button uk-button-primary" type="button" >Select</button>
+        <button class="uk-button uk-button-primary" type="button" uk-toggle="target: #modal-imageUploader">Upload</button>
 
         {:else}
             <center>
                 <img src="https://faviconer.net/preloaders/25/Snake.gif" alt="Loading...">
             </center>
         {/if}
-       <div class="uk-inline">
-            <button class="uk-button uk-button-default" type="button">Click</button>
-            <div uk-dropdown="mode: click">
+       <div class="uk-align-right uk-inline">
+            <button class="uk-button uk-button-default" type="button">{currentFilter}</button>
+            <div uk-dropdown="mode:click">
              <ul class="uk-nav uk-dropdown-nav">
-                <li><a href="#">Date</a></li>
-                <li><a href="#">Company</a></li>
-                <li><a href="#">Project</a></li>
-                <li><a href="#">Location</a></li>
-                <li><a href="#">TP #</a></li>
-                <li><a href="#">People</a></li>
+                <li><a on:click={() => setFilter('Date')} href="#">Date</a></li>
+                <li><a on:click={() => setFilter('Company')} href="#">Company</a></li>
+                <li><a on:click={() => setFilter('Project')} href="#">Project</a></li>
+                <li><a on:click={() => setFilter('Location')} href="#">Location</a></li>
+                <li><a on:click={() => setFilter('TP')} href="#">TP #</a></li>
+                <li><a on:click={() => setFilter('People')} href="#">People</a></li>
                 </ul>
             </div>
         </div>
-        <form class="uk-search uk-search-default">
-            <span uk-search-icon></span>
-            <input class="uk-search-input" type="search" placeholder="">
+            <form class="uk-align-right uk-search uk-search-default">
+            <a uk-search-icon on:click={filterList}></a>
+            <input class="uk-search-input " bind:value={searchParam} placeholder=""/>
         </form> 
         <table class="uk-table uk-table-divider">
             <thead>
@@ -132,4 +150,49 @@
                 </div>
             </div>
         </div>
+
+     
+        <div id="modal-imageUpload" uk-modal>
+            <div class="uk-modal-dialog">
+                <button class="uk-modal-close-default" type="button" uk-close></button>
+                <div class="uk-modal-header">
+                    <h2 class="uk-modal-title">Account Creation</h2>
+                </div>
+                <div class="uk-modal-body" uk-overflow-auto>
+                  
+                    <form>
+                        <div class="uk-margin">
+                            <div class="uk-inline uk-width-1-1">
+                                <span class="uk-form-icon" uk-icon="icon: user"></span>
+                                <input class="uk-input uk-form-large" placeholder="First Name" type="text">
+                            </div>
+                        </div>
+                        <div class="uk-margin">
+                            <div class="uk-inline uk-width-1-1">
+                                <span class="uk-form-icon" uk-icon="icon: users"></span>
+                                <input class="uk-input uk-form-large"  placeholder="Last Name" type="text">
+                            </div>
+                        </div>
+                        <div class="uk-margin">
+                            <div class="uk-inline uk-width-1-1">
+                                <span class="uk-form-icon" uk-icon="icon: user"></span>
+                                <input class="uk-input uk-form-large"  placeholder="Username" type="text">
+                            </div>
+                        </div>
+                        <div class="uk-margin">
+                            <div class="uk-inline uk-width-1-1">
+                                <span class="uk-form-icon" uk-icon="icon: lock"></span>
+                                <input class="uk-input uk-form-large"   placeholder="Password" type="password">	
+                            </div>
+                        </div>
+                </div>
+
+                <div class="uk-modal-footer uk-text-right">
+                    <button class="uk-button uk-button-default uk-modal-close" type="button">Cancel</button>
+                    <button class="uk-button uk-button-primary" type="button">Create</button>
+                </div>
+
+            </div>
+        </div>
+    
 </main>
